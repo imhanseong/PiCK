2a5ca4847a7ed7a536e3fc5e46430bafc22cf26b
imhanseong <hans_hanseong@dsm.hs.kr>
2024-05-07 18:57:37 +0900

    pick 멘토링

---
+++ src/main/java/com/example/pick_study/PiCkStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pick_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCkStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiCkStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pick_study/PiCkStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pick_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiCkStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
